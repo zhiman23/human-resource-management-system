15aca29a027e21c525ce97ba6f2305b456dfde1e
yxy <2519675951@qq.com>
2020-12-10 19:52:49 +0800

    更改路径

--- src/api/user.js
+++ src/api/user.js
@@ -2,11 +2,11 @@ import request from '@/utils/request'
 
 // import axios from '@/utils/request'
 export function login(data) {
-    return request({
-        method: 'post',
-        url: '/api/sys/login',
-        data
-    })
+  return request({
+    method: 'post',
+    url: '/sys/login',
+    data
+  })
 }
 
 export function getInfo(token) {
