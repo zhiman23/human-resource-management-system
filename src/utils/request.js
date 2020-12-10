dbc1638dce578e33ab3d9650f97d581418c02f33
yxy <2519675951@qq.com>
2020-12-10 19:54:08 +0800

    添加响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,24 @@
 import axios from 'axios'
+import { Message } from 'element-ui'
 
-const service = axios.create()
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
 service.interceptors.request.use()
-service.interceptors.response.use()
+// 响应拦截器
+service.interceptors.response.use(res => {
+  console.log(res.data)
+  const { message, success, data } = res.data
+  if (success) {
+    return data
+  } else {
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+}, err => {
+  Message.error(err.message)
+  return Promise.reject(err.message)
+})
 
 export default service
- 
