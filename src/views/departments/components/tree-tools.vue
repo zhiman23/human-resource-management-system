35ea8ff6b9bd2308cf102e6c20b659089762bc41
yxy <2519675951@qq.com>
2020-12-14 20:26:51 +0800

    添加子部门

--- src/views/departments/components/tree-tools.vue
+++ src/views/departments/components/tree-tools.vue
@@ -9,15 +9,15 @@
         <el-col>{{ data.manger }}</el-col>
         <el-col>
           <!-- 下拉菜单 element -->
-          <el-dropdown>
+          <el-dropdown @command="operateDepts">
             <span>
               操作<i class="el-icon-arrow-down" />
             </span>
             <!-- 下拉菜单 -->
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>添加子部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">查看部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+              <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="edit">查看部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="del">删除部门</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -27,6 +27,8 @@
 </template>
 
 <script>
+import { delDepartments } from '@/api/departments'
+
 export default {
   props: {
     data: {
@@ -35,6 +37,32 @@ export default {
     },
     isRoot: {
       type: Boolean
+    }
+  },
+  methods: {
+    operateDepts(option) {
+      if (option === 'add') {
+        this.$emit('addDepts', this.data)
+      }
+      if (option === 'edit') {
+        //
+      }
+      if (option === 'del') {
+        this.$confirm('是否确认删除该部门', '删除部门', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          delDepartments(this.data.id).then(() => {
+            console.log('删除成功')
+            this.$emit('delDepts')
+          }).catch(() => {
+            console.log('删除失败')
+          })
+        }).catch(() => {
+
+        })
+      }
     }
   }
 }
