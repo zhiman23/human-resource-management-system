931c3fdc1be869d5835c7ce8c84fd245a92c9621
yxy <2519675951@qq.com>
2020-12-14 20:26:17 +0800

    格式化首页

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,53 +1,76 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-
       <el-card class="tree-card">
         <tree-tools :data="company" :is-root="true" />
         <hr>
-        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-
-          <tree-tools slot-scope="{data}" :data="data" />
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          :default-expand-all="true"
+        >
+          <tree-tools
+            slot-scope="{ data }"
+            :data="data"
+            @addDepts="addDepts"
+            @delDepts="loadPage"
+          />
         </el-tree>
       </el-card>
     </div>
+    <addDept :show-dialog.sync="showDialog" :data="node" @addDepts="loadPage" />
   </div>
 </template>
 
 <script>
 import treeTools from './components/tree-tools.vue'
+import addDept from './components/add-dept'
 import { getDepartments } from '@/api/departments'
 import { convertTreeData } from '@/utils/auth'
+
 export default {
-  components: { treeTools },
+  components: {
+    treeTools,
+    addDept
+  },
   data() {
     return {
+      showDialog: false,
       departs: [],
       defaultProps: {
         label: 'name',
         children: 'children'
       },
-      company: {}
+      company: {},
+      node: {}
     }
   },
   created() {
-    getDepartments().then(res => {
-      console.log(res)
-      this.company = {
-        name: res.companyName,
-        manger: '负责人'
-      }
-      // this.departs = res.depts
-      this.departs = convertTreeData(res.depts, '')
-    })
+    this.loadPage()
+  },
+  methods: {
+    loadPage() {
+      getDepartments().then((res) => {
+        console.log(res)
+        this.company = {
+          name: res.companyName,
+          manager: '负责人',
+          id: ''
+        }
+        this.departs = convertTreeData(res.depts, '')
+      })
+    },
+    addDepts(node) {
+      this.showDialog = true
+      this.node = node
+    }
   }
-
 }
 </script>
 
 <style scoped>
 .tree-card {
-  padding: 30px  140px;
-  font-size:14px;
+  padding: 30px 140px;
+  font-size: 14px;
 }
 </style>
