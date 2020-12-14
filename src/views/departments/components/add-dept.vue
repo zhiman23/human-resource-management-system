74da36c7d8407e432f285e7c20cf5df47240e6b7
yxy <2519675951@qq.com>
2020-12-14 21:11:22 +0800

    格式化代码

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -4,13 +4,26 @@
     <!-- 匿名插槽 -->
     <el-form ref="form" label-width="120px" :model="formData" :rules="rules">
       <el-form-item label="部门名称" prop="name">
-        <el-input v-model="formData.name" style="width:80%" placeholder="1-50个字符" />
+        <el-input
+          v-model="formData.name"
+          style="width: 80%"
+          placeholder="1-50个字符"
+        />
       </el-form-item>
       <el-form-item label="部门编码" prop="code">
-        <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符" />
+        <el-input
+          v-model="formData.code"
+          style="width: 80%"
+          placeholder="1-50个字符"
+        />
       </el-form-item>
       <el-form-item label="部门负责人" prop="manager">
-        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" @focus="getEmployeeSimple">
+        <el-select
+          v-model="formData.manager"
+          style="width: 80%"
+          placeholder="请选择"
+          @focus="getEmployeeSimple"
+        >
           <el-option
             v-for="item in people"
             :key="item.id"
@@ -20,7 +33,13 @@
         </el-select>
       </el-form-item>
       <el-form-item label="部门介绍" prop="introduce">
-        <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+        <el-input
+          v-model="formData.introduce"
+          style="width: 80%"
+          placeholder="1-300个字符"
+          type="textarea"
+          :rows="3"
+        />
       </el-form-item>
     </el-form>
 
@@ -51,19 +70,19 @@ export default {
   },
   data() {
     const validateDeptsName = (rule, value, callback) => {
-      getDepartments().then(data => {
+      getDepartments().then((data) => {
         const { depts } = data
         // const existed = false
-        depts.some(item => item.name === value && item.pid === this.data.id)
+        depts.some((item) => item.name === value && item.pid === this.data.id)
           ? callback(new Error('同一个部门下不能重名'))
           : callback()
       })
     }
 
     const validateDeptsCode = (rule, value, callback) => {
-      getDepartments().then(data => {
+      getDepartments().then((data) => {
         const { depts } = data
-        depts.some(item => item.code === value && value)
+        depts.some((item) => item.code === value && value)
           ? callback(new Error('不能有重复的部门代码'))
           : callback()
       })
@@ -81,12 +100,22 @@ export default {
       rules: {
         name: [
           { required: true, message: '部门名称不能为空', trigger: 'blur' },
-          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' },
+          {
+            min: 1,
+            max: 50,
+            message: '部门名称要求1-50个字符',
+            trigger: 'blur'
+          },
           { trigger: 'blur', validator: validateDeptsName }
         ],
         code: [
           { required: true, message: '部门编码不能为空', trigger: 'blur' },
-          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' },
+          {
+            min: 1,
+            max: 50,
+            message: '部门编码要求1-50个字符',
+            trigger: 'blur'
+          },
           { trigger: 'blur', validator: validateDeptsCode }
         ],
         manager: [
@@ -125,11 +154,14 @@ export default {
     btnCancel() {
       this.$refs.form.resetFields()
       this.$emit('update:showDialog', false)
+    },
+    // 数据回显
+    getDepartmentDetails() {
+      this.formData = this.data
     }
   }
 }
 </script>
 
 <style>
-
 </style>
