1c4e60a15d3b0c571d19909b6567cc0b6a713680
yxy <2519675951@qq.com>
2020-12-14 22:29:54 +0800

    公司设置tab栏

---
+++ src/views/setting/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="dashbord-container">
+    <div class="app-container">
+      <el-card>
+        <el-tabs v-model="activeName">
+          <el-tab-pane name="role" label="角色管理">
+            角色管理内容
+          </el-tab-pane>
+          <el-tab-pane name="company" label="公司信息">
+            公司信息内容
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeName: 'role'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
