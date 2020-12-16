077cd0a0d42460fa83f1dac3ae0230265874e6ba
yxy <2519675951@qq.com>
2020-12-16 11:31:33 +0800

    员工页面的基本布局

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <PageTools :show-before="true">
+        <span slot="before">共166条记录</span>
+        <template slot="after">
+          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </PageTools>
+      <el-card>
+        <el-table border>
+          <el-table-column label="序号" sortable="" />
+          <el-table-column label="姓名" sortable="" />
+          <el-table-column label="工号" sortable="" />
+          <el-table-column label="聘用形式" sortable="" />
+          <el-table-column label="部门" sortable="" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="账户状态" sortable="" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-row type="flex" justify="end" align="middle" style="height: 60px">
+          <el-pagination layout="prev, pager, next" :total="80" /> </el-row></el-card>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: [
+        { name: '炎炎' }
+      ]
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
