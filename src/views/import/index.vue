10e5f01999b8e98d367ca8f5b817f60404585a53
yxy <2519675951@qq.com>
2020-12-18 17:26:14 +0800

    导入员工数据时处理日期对象

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -24,19 +24,36 @@ export default {
         return this.cnToEn(item, dictionary)
       })
       await importEmployees(data)
-      console.log(data)
     },
     cnToEn(item, dictionary) {
       const newItem = {}
-      console.log(item)
+      // item是员工
       for (const key in item) {
         const newKey = dictionary[key]
-        const value = item[key]
-        // console.log(newKey)
-        // console.log(value)
+        // const value = item[key]
+        let value = item[key]
+        if (newKey === 'timeOfEntry' || newKey === 'correctionTime') {
+          const formatedDateStr = this.formatDate(value, '/')
+          value = new Date(formatedDateStr)
+        }
         newItem[newKey] = value
       }
       return newItem
+    },
+    formatDate(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1)
+      time.setYear(time.getFullYear() - 70)
+      const year = time.getFullYear() + ''
+      const month = time.getMonth() + 1 + ''
+      const date = time.getDate() - 1 + ''
+      if (format && format.length === 1) {
+        return year + format + month + format + date
+      }
+      return (
+        year +
+        (month < 10 ? '0' + month : month) +
+        (date < 10 ? '0' + date : date)
+      )
     }
   }
 }
