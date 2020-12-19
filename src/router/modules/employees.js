0886266d4e94a0f526a2e3255fe6d1a8fa5da213
yxy <2519675951@qq.com>
2020-12-19 20:58:23 +0800

    创建员工详情页

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -2,9 +2,19 @@ import Layout from '@/layout'
 export default {
   path: '/employees',
   component: Layout,
-  children: [{
-    path: '',
-    component: () => import('@/views/employees/index'),
-    meta: { title: '员工', icon: 'people' }
-  }]
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/employees/index'),
+      meta: { title: '员工', icon: 'people' }
+    },
+    {
+      path: 'detail/:id?',
+      component: () => import('@/views/employees/detail'),
+      hidden: true,
+      meta: {
+        title: '员工详情'
+      }
+    }
+  ]
 }
---
+++ src/views/employees/detail/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card>
+        <el-tabs v-model="activeName">
+          <el-tab-pane label="登录账户设置" name="account">
+            666
+          </el-tab-pane>
+          <el-tab-pane label="个人详情" name="detail">
+            777
+          </el-tab-pane>
+          <el-tab-pane label="岗位信息" name="job">
+            888
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeName: 'account'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
