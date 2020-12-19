3108aae2a70bd12ecae71f44836f339818bec289
yxy <2519675951@qq.com>
2020-12-19 22:10:58 +0800

    封装个人信息、岗位信息接口（读取、更改）

--- src/api/employees.js
+++ src/api/employees.js
@@ -47,3 +47,35 @@ export function saveUserDetailById(data) {
     data
   })
 }
+
+// 读取员工的基本信息
+export function getPersonalDetail(id) {
+  return request({
+    url: `/employees/${id}/personalInfo`
+  })
+}
+
+// 更新用户详情的基础信息
+export function updatePersonal(data) {
+  return request({
+    url: `/employees/${data.userId}/personalInfo`,
+    method: 'put',
+    data
+  })
+}
+
+// 获取员工岗位信息
+export function getJobDetail(id) {
+  return request({
+    url: `/employees/${id}/jobs`
+  })
+}
+
+// 保存员工岗位信息
+export function updateJob(data) {
+  return request({
+    url: `/employees/${data.userId}/jobs`,
+    method: 'put',
+    data
+  })
+}
