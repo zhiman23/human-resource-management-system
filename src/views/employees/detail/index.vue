e2c15251ba5dd410c48cf62cef07eea2cc133515
yxy <2519675951@qq.com>
2020-12-19 21:38:40 +0800

    账号信息回显

--- src/views/employees/detail/index.vue
+++ src/views/employees/detail/index.vue
@@ -4,14 +4,23 @@
       <el-card>
         <el-tabs v-model="activeName">
           <el-tab-pane label="登录账户设置" name="account">
-            666
+            <el-row type="flex" justify="center" style="padding-right: 300px">
+              <el-form label-width="100px">
+                <el-form-item label="姓名">
+                  <el-input v-model="formData.username" />
+                </el-form-item>
+                <el-form-item label="密码">
+                  <el-input v-model="formData.newPassword" type="password" />
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary">更新</el-button>
+                  <el-button>取消</el-button>
+                </el-form-item>
+              </el-form>
+            </el-row>
           </el-tab-pane>
-          <el-tab-pane label="个人详情" name="detail">
-            777
-          </el-tab-pane>
-          <el-tab-pane label="岗位信息" name="job">
-            888
-          </el-tab-pane>
+          <el-tab-pane label="个人详情" name="detail"> 777 </el-tab-pane>
+          <el-tab-pane label="岗位信息" name="job"> 888 </el-tab-pane>
         </el-tabs>
       </el-card>
     </div>
@@ -19,10 +28,27 @@
 </template>
 
 <script>
+import { getUserDetailById } from '@/api/user'
+
 export default {
   data() {
     return {
-      activeName: 'account'
+      userId: this.$route.params.id,
+      activeName: 'account',
+      formData: {
+        username: '',
+        newPassword: ''
+      }
+    }
+  },
+  created() {
+    this.getUserDetailById()
+  },
+  methods: {
+    async getUserDetailById() {
+      const data = await getUserDetailById(this.userId)
+      console.log(data)
+      this.formData = data
     }
   }
 }
