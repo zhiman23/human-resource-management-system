9a302c8ba796f920de26d0df0f1a0e9da7658428
yxy <2519675951@qq.com>
2020-12-19 21:25:11 +0800

    保存员工信息请求

--- src/api/employees.js
+++ src/api/employees.js
@@ -38,3 +38,12 @@ export function importEmployees(data) {
     data
   })
 }
+
+// 保存员工的基本信息
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
