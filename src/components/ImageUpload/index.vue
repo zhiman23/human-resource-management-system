a29d5e65cae549a58c86a479f06a6002892458f0
yxy <2519675951@qq.com>
2020-12-20 20:03:48 +0800

    封装图片上传组件，根据数量决定是否显示上传按钮

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <el-upload
+      action="#"
+      list-type="picture-card"
+      :on-preview="preview"
+      :file-list="fileList"
+      :class="{disable:disableUpload}"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog :visible.sync="showDialog" title="图片预览">
+      <img :src="imgUrl" alt="">
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showDialog: false,
+      fileList: [
+        {
+          url:
+            'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=99674480,3698416910&fm=15&gp=0.jpg'
+        }
+      ],
+      imgUrl: ''
+    }
+  },
+  computed: {
+    disableUpload() {
+      return this.fileList.length === 1
+    }
+  },
+  methods: {
+    preview(file) {
+      // 放大镜🔍事件
+      this.imgUrl = file.url
+      this.showDialog = true
+      console.log(file)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.disable{
+ ::v-deep .el-upload--picture-card{
+        display: none;
+    }
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 // 创建Vue.use 注册组件
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
