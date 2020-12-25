09c39e297634dc6b9631af1fe74dac34a6e9a6c5
yxy <2519675951@qq.com>
2020-12-25 20:19:14 +0800

    生成年份选择器

---
+++ src/views/dashboard/components/work-calender.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-select>
+      <el-option v-for="(item, index) in yearList" :key="index" :value="item" :label="item+'年'" />
+    </el-select>
+    <el-select>
+      <el-option v-for="item in 12" :key="item" :value="item" :label="item+'月'" />
+    </el-select>
+    <el-calendar v-model="currentDate" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    startDate: {
+      type: Date,
+      default: () => new Date()
+    }
+  },
+  data() {
+    return {
+      currentDate: null,
+      yearList: []
+    }
+  },
+  created() {
+    this.yearList = Array.from(Array(11), (val, index) => {
+      const now = new Date()
+      return index + now.getFullYear() - 5
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
