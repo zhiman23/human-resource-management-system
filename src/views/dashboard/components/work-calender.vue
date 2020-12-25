d6402c3c871e722ed7612ffd63b3b4730242b79a
yxy <2519675951@qq.com>
2020-12-25 21:19:48 +0800

    日历日期变更功能实现

--- src/views/dashboard/components/work-calender.vue
+++ src/views/dashboard/components/work-calender.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <el-select>
-      <el-option v-for="(item, index) in yearList" :key="index" :value="item" :label="item+'年'" />
-    </el-select>
-    <el-select>
-      <el-option v-for="item in 12" :key="item" :value="item" :label="item+'月'" />
-    </el-select>
+    <el-row type="flex" justify="end">
+      <el-select v-model="currentYear" size="mini" @change="changeDate">
+        <el-option v-for="(item, index) in yearList" :key="index" :value="item" :label="item+'年'" />
+      </el-select>
+      <el-select v-model="currentMonth" size="mini" @change="changeDate">
+        <el-option v-for="item in 12" :key="item" :value="item" :label="item+'月'" />
+      </el-select>
+    </el-row>
     <el-calendar v-model="currentDate" />
   </div>
 </template>
@@ -20,15 +22,29 @@ export default {
   },
   data() {
     return {
+      currentMonth: null,
+      currentYear: null,
       currentDate: null,
       yearList: []
     }
   },
   created() {
+    // 获取默认的当前年份和当前月份
+    this.currentYear = this.startDate.getFullYear()
+    this.currentMonth = this.startDate.getMonth() + 1
+    // 获取前后五年的年份数组
     this.yearList = Array.from(Array(11), (val, index) => {
       const now = new Date()
       return index + now.getFullYear() - 5
     })
+  },
+  methods: {
+    changeDate() {
+      const year = this.currentYear
+      const month = this.currentMonth
+      const dateStr = `${year}-${month}-1`
+      this.currentDate = new Date(dateStr)
+    }
   }
 }
 </script>
